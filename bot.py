930933deb84cd14088a0181bc260443632ec5c11
0xMonnn <imankurniawan1933@gmail.com>
2025-03-06 18:54:31 +0700

    first commit

---
+++ bot.py
@@ -0,0 +1,410 @@
+from aiohttp import (
+    ClientResponseError,
+    ClientSession,
+    ClientTimeout
+)
+from aiohttp_socks import ProxyConnector
+from fake_useragent import FakeUserAgent
+from datetime import datetime
+from colorama import Fore, Style, init
+import asyncio, random, json, os, pytz
+
+# Initialize colorama
+init(autoreset=True)
+
+wib = pytz.timezone('Asia/Jakarta')
+
+def welcome():
+    print(f"{Fore.CYAN + Style.BRIGHT} █████╗ ███████╗██╗   ██╗███╗   ██╗ ██████╗")
+    print(f"{Fore.CYAN + Style.BRIGHT}██╔══██╗██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝")
+    print(f"{Fore.CYAN + Style.BRIGHT}███████║███████╗ ╚████╔╝ ██╔██╗ ██║██║      ")
+    print(f"{Fore.CYAN + Style.BRIGHT}██╔══██║╚════██║  ╚██╔╝  ██║╚██╗██║██║      ")
+    print(f"{Fore.CYAN + Style.BRIGHT}██║  ██║███████║   ██║   ██║ ╚████║╚██████╔ ")
+    print(f"{Fore.CYAN + Style.BRIGHT}╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝ ")
+    print(f"{Fore.GREEN + Style.BRIGHT}               JOIN AIRDROP         ")
+    print(f"{Fore.YELLOW + Style.BRIGHT}      https://t.me/AirdropSyncReborn ")
+    print(Style.RESET_ALL)  # Mengembalikan warna terminal ke default
+
+class OpenLoop:
+    def __init__(self) -> None:
+        self.headers = {
+            "Accept": "*/*",
+            "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Origin": "chrome-extension://effapmdildnpkiaeghlkicpfflpiambm",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "cross-site",
+            "User-Agent": FakeUserAgent().random
+        }
+        self.proxies = []
+        self.proxy_index = 0
+        self.account_proxies = {}
+
+    def clear_terminal(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def log(self, message):
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}{message}",
+            flush=True
+        )
+
+    def format_seconds(self, seconds):
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+    
+    def load_accounts(self):
+        filename = "accounts.json"
+        try:
+            if not os.path.exists(filename):
+                self.log(f"{Fore.RED}File {filename} Not Found.{Style.RESET_ALL}")
+                return
+
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                if isinstance(data, list):
+                    return data
+                return []
+        except json.JSONDecodeError:
+            return []
+    
+    async def load_proxies(self, use_proxy_choice: bool):
+        filename = "proxy.txt"
+        try:
+            if use_proxy_choice == 1:
+                async with ClientSession(timeout=ClientTimeout(total=30)) as session:
+                    async with session.get("https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt") as response:
+                        response.raise_for_status()
+                        content = await response.text()
+                        with open(filename, 'w') as f:
+                            f.write(content)
+                        self.proxies = content.splitlines()
+            else:
+                if not os.path.exists(filename):
+                    self.log(f"{Fore.RED + Style.BRIGHT}File {filename} Not Found.{Style.RESET_ALL}")
+                    return
+                with open(filename, 'r') as f:
+                    self.proxies = f.read().splitlines()
+            
+            if not self.proxies:
+                self.log(f"{Fore.RED + Style.BRIGHT}No Proxies Found.{Style.RESET_ALL}")
+                return
+
+            self.log(
+                f"{Fore.GREEN + Style.BRIGHT}Proxies Total  : {Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT}{len(self.proxies)}{Style.RESET_ALL}"
+            )
+        
+        except Exception as e:
+            self.log(f"{Fore.RED + Style.BRIGHT}Failed To Load Proxies: {e}{Style.RESET_ALL}")
+            self.proxies = []
+
+    def check_proxy_schemes(self, proxies):
+        schemes = ["http://", "https://", "socks4://", "socks5://"]
+        if any(proxies.startswith(scheme) for scheme in schemes):
+            return proxies
+        return f"http://{proxies}"
+
+    def get_next_proxy_for_account(self, email):
+        if email not in self.account_proxies:
+            if not self.proxies:
+                return None
+            proxy = self.check_proxy_schemes(self.proxies[self.proxy_index])
+            self.account_proxies[email] = proxy
+            self.proxy_index = (self.proxy_index + 1) % len(self.proxies)
+        return self.account_proxies[email]
+
+    def rotate_proxy_for_account(self, email):
+        if not self.proxies:
+            return None
+        proxy = self.check_proxy_schemes(self.proxies[self.proxy_index])
+        self.account_proxies[email] = proxy
+        self.proxy_index = (self.proxy_index + 1) % len(self.proxies)
+        return proxy
+    
+    def mask_account(self, account):
+        if "@" in account:
+            local, domain = account.split('@', 1)
+            mask_account = local[:3] + '*' * 3 + local[-3:]
+            return f"{mask_account}@{domain}"
+    
+    def print_message(self, email, proxy, color, message):
+        self.log(
+            f"{Fore.CYAN + Style.BRIGHT}[ Account:{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} {self.mask_account(email)} {Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT} Proxy: {Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT}{proxy}{Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT} - {Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT}Status:{Style.RESET_ALL}"
+            f"{color + Style.BRIGHT} {message} {Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT}]{Style.RESET_ALL}"
+        )
+
+    def print_question(self):
+        while True:
+            try:
+                print("1. Run With Monosans Proxy")
+                print("2. Run With Private Proxy")
+                print("3. Run Without Proxy")
+                choose = int(input("Choose [1/2/3] -> ").strip())
+
+                if choose in [1, 2, 3]:
+                    proxy_type = (
+                        "Run With Monosans Proxy" if choose == 1 else 
+                        "Run With Private Proxy" if choose == 2 else 
+                        "Run Without Proxy"
+                    )
+                    print(f"{Fore.GREEN + Style.BRIGHT}{proxy_type} Selected.{Style.RESET_ALL}")
+                    return choose
+                else:
+                    print(f"{Fore.RED + Style.BRIGHT}Please enter either 1, 2 or 3.{Style.RESET_ALL}")
+            except ValueError:
+                print(f"{Fore.RED + Style.BRIGHT}Invalid input. Enter a number (1, 2 or 3).{Style.RESET_ALL}")
+    
+    async def user_login(self, email: str, password: str, proxy=None):
+        url = "https://api.openloop.so/users/login"
+        data = json.dumps({"username":email, "password":password})
+        headers = {
+            **self.headers,
+            "Authorization": "Bearer",
+            "Content-Length": str(len(data)),
+            "Content-Type": "text/plain;charset=UTF-8",
+        }
+        connector = ProxyConnector.from_url(proxy) if proxy else None
+        try:
+            async with ClientSession(connector=connector, timeout=ClientTimeout(total=60)) as session:
+                async with session.post(url=url, headers=headers, data=data) as response:
+                    response.raise_for_status()
+                    result = await response.json()
+                    return result['data']['accessToken']
+        except (Exception, ClientResponseError) as e:
+            return self.print_message(email, proxy, Fore.RED, f"GET Access Token Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
+        
+    async def mission_lists(self, email: str, password: str, token: str, use_proxy: bool, proxy=None, retries=5):
+        url = "https://api.openloop.so/missions"
+        headers = {
+            **self.headers,
+            "Authorization": f"Bearer {token}",
+        }
+        for attempt in range(retries):
+            connector = ProxyConnector.from_url(proxy) if proxy else None
+            try:
+                async with ClientSession(connector=connector, timeout=ClientTimeout(total=60)) as session:
+                    async with session.get(url=url, headers=headers) as response:
+                        if response.status == 401:
+                            token = await self.get_access_token(email, password, use_proxy)
+                            headers["Authorization"] = f"Bearer {token}"
+                            continue
+
+                        response.raise_for_status()
+                        result = await response.json()
+                        return result['data']['missions']
+            except (Exception, ClientResponseError) as e:
+                if attempt < retries - 1:
+                    await asyncio.sleep(5)
+                    continue
+                
+                return self.print_message(email, proxy, Fore.RED, f"GET Available Mission Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
+        
+    async def complete_missions(self, email: str, password: str, token: str, mission_id: int, use_proxy: bool, proxy=None, retries=5):
+        url = f"https://api.openloop.so/missions/{mission_id}/complete"
+        headers = {
+            **self.headers,
+            "Authorization": f"Bearer {token}",
+        }
+        for attempt in range(retries):
+            connector = ProxyConnector.from_url(proxy) if proxy else None
+            try:
+                async with ClientSession(connector=connector, timeout=ClientTimeout(total=60)) as session:
+                    async with session.get(url=url, headers=headers) as response:
+                        if response.status == 401:
+                            token = await self.get_access_token(email, password, use_proxy)
+                            headers["Authorization"] = f"Bearer {token}"
+                            continue
+
+                        response.raise_for_status()
+                        return await response.json()
+            except (Exception, ClientResponseError) as e:
+                if attempt < retries - 1:
+                    await asyncio.sleep(5)
+                    continue
+                
+                return self.print_message(email, proxy, Fore.RED, f"Complete Available Mission Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
+            
+    async def send_ping(self, email: str, password: str, token: str, quality: int, use_proxy: bool, proxy=None, retries=5):
+        url = "https://api.openloop.so/bandwidth/share"
+        data = json.dumps({"quality":quality})
+        headers = {
+            **self.headers,
+            "Authorization": f"Bearer {token}",
+            "Content-Length": str(len(data)),
+            "Content-Type": "application/json",
+        }
+        for attempt in range(retries):
+            connector = ProxyConnector.from_url(proxy) if proxy else None
+            try:
+                async with ClientSession(connector=connector, timeout=ClientTimeout(total=60)) as session:
+                    async with session.post(url=url, headers=headers, data=data) as response:
+                        if response.status == 401:
+                            token = await self.get_access_token(email, password, use_proxy)
+                            headers["Authorization"] = f"Bearer {token}"
+                            continue
+
+                        response.raise_for_status()
+                        result = await response.json()
+                        return result['data']
+            except (Exception, ClientResponseError) as e:
+                if attempt < retries - 1:
+                    await asyncio.sleep(5)
+                    continue
+
+                self.print_message(email, proxy, Fore.RED, f"PING Failed: {Fore.YELLOW+Style.BRIGHT}{str(e)}")
+
+                if "invalid proxy response" in str(e).lower():
+                    proxy = self.rotate_proxy_for_account(email) if use_proxy else None
+
+                return None
+            
+    async def get_access_token(self, email: str, password: str, use_proxy: bool):
+        proxy = self.get_next_proxy_for_account(email) if use_proxy else None
+        token = None
+        while token is None:
+            token = await self.user_login(email, password, proxy)
+            if not token:
+                proxy = self.rotate_proxy_for_account(email) if use_proxy else None
+                continue
+            
+            self.print_message(email, proxy, Fore.GREEN, "GET Access Token Success")
+            return token
+
+    async def process_complete_missions(self, email: str, password: str, token: str, use_proxy: bool):
+        while True:
+            proxy = self.get_next_proxy_for_account(email) if use_proxy else None
+            missions = await self.mission_lists(email, password, token, use_proxy, proxy)
+            if missions:
+                completed = False
+                for mission in missions:
+                    mission_id = str(mission['missionId'])
+                    title = mission['name']
+                    reward = mission['reward']['amount']
+                    type = mission['reward']['type']
+                    status = mission['status']
+
+                    if mission and status == "available":
+                        complete = await self.complete_missions(email, password, token, mission_id, use_proxy, proxy)
+                        if "message" in complete and complete['message'] == "Success":
+                            self.print_message(email, proxy, Fore.WHITE, 
+                                f"Mission {title}"
+                                f"{Fore.GREEN + Style.BRIGHT} Is Completed {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                                f"{Fore.CYAN + Style.BRIGHT} Reward: {Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT}{reward} {type}{Style.RESET_ALL}"
+                            )
+                        else:
+                            self.print_message(email, proxy, Fore.WHITE, 
+                                f"Mission {title} "
+                                f"{Fore.RED + Style.BRIGHT}Isn't Completed{Style.RESET_ALL}"
+                            )
+                        await asyncio.sleep(1)
+
+                    else:
+                        completed = True
+
+                if completed:
+                    self.print_message(email, proxy, Fore.GREEN, 
+                        "All Available Mission Is Completed"
+                    )
+
+            await asyncio.sleep(24 * 60 * 60)
+
+    async def process_send_ping(self, email: str, password: str, token: str, use_proxy: bool):
+        quality = random.randint(60, 80)
+        while True:
+            proxy = self.get_next_proxy_for_account(email) if use_proxy else None
+            print(
+                f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                f"{Fore.BLUE + Style.BRIGHT}Try to Sent Ping...{Style.RESET_ALL}                                         ",
+                end="\r",
+                flush=True
+            )
+
+            ping = await self.send_ping(email, password, token, quality, use_proxy, proxy)
+            if ping:
+                total_earning = ping.get('balances', {}).get('POINT', 0)
+                self.print_message(email, proxy, Fore.GREEN, 
+                    "PING Success "
+                    f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                    f"{Fore.CYAN + Style.BRIGHT} Earning: {Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT}Total {total_earning:.2f} PTS{Style.RESET_ALL}"
+                )
+
+            print(
+                f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                f"{Fore.BLUE + Style.BRIGHT}Wait For 3 Minutes For Next Ping...{Style.RESET_ALL}",
+                end="\r"
+            )
+            await asyncio.sleep(3 * 60)
+            
+    async def process_accounts(self, email: str, password: str, use_proxy: bool):
+        token = await self.get_access_token(email, password, use_proxy)
+        if token:
+            tasks = []
+            tasks.append(self.process_complete_missions(email, password, token, use_proxy))
+            tasks.append(self.process_send_ping(email, password, token, use_proxy))
+            await asyncio.gather(*tasks)
+    
+    async def main(self):
+        try:
+            accounts = self.load_accounts()
+            if not accounts:
+                self.log(f"{Fore.RED + Style.BRIGHT}No Accounts Loaded.{Style.RESET_ALL}")
+                return
+            
+            use_proxy_choice = self.print_question()
+
+            use_proxy = False
+            if use_proxy_choice in [1, 2]:
+                use_proxy = True
+
+            self.clear_terminal()
+            welcome()
+            self.log(
+                f"{Fore.GREEN + Style.BRIGHT}Account's Total: {Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT}{len(accounts)}{Style.RESET_ALL}"
+            )
+
+            if use_proxy:
+                await self.load_proxies(use_proxy_choice)
+
+            self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+
+            while True:
+                tasks = []
+                for account in accounts:
+                    email = account.get('Email')
+                    password = account.get('Password')
+
+                    if "@" in email and password:
+                        tasks.append(self.process_accounts(email, password, use_proxy))
+
+                await asyncio.gather(*tasks)
+                await asyncio.sleep(10)
+
+        except Exception as e:
+            self.log(f"{Fore.RED+Style.BRIGHT}Error: {e}{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+    try:
+        bot = OpenLoop()
+        asyncio.run(bot.main())
+    except KeyboardInterrupt:
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.RED + Style.BRIGHT}[ EXIT ] OpenLoop - BOT{Style.RESET_ALL}                                       "                              
+        )
